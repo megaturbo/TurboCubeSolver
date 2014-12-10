e32f5c2fd61a43310da2e0b8eff80e4eda74d151
Quentin Jeanmonod <quentin.jeanmonod@he-arc.ch>
2014-12-10 11:47:23 +0100

    first version of fridrich cross and modified cube methods a bit

--- code/TURBOCUBEsolver/Fridrich.cpp
+++ code/TURBOCUBEsolver/Fridrich.cpp
@@ -5,6 +5,14 @@
 #include <qdebug.h>
 
 QString Fridrich::solve(Cube *c){
+//    color solvedMatrix[18][3];
+
+//    for (int x = 0; x < 18; ++x) {
+//        for (int y = 0; y < 3; ++y) {
+//            solvedMatrix[x][y] = (color)(x / 3);
+//        }
+//    }
+//    Cube solvedCube(solvedMatrix);
     QString s;
     s = cross(c);
     s += F2L(c);
@@ -14,12 +22,94 @@ QString Fridrich::solve(Cube *c){
 }
 
 //For now, it's only solving for the white face
+//can be optimized
 QString Fridrich::cross(Cube *c){
-    QList<int> wrpos = c->locateCubie(WHITE, RED);
-    QList<int> wbpos = c->locateCubie(WHITE, BLUE);
-    QList<int> wopos = c->locateCubie(WHITE, ORANGE);
-    QList<int> wgpos = c->locateCubie(WHITE, GREEN);
-    return "";
+    QString s; //string return
+    for (int col = 0; col < 4; col++) {
+        QList<int> pos = c->locateCubie(WHITE, (color) col);
+        color faceWhite = (color) (pos.at(0) / 3);
+        color faceCol = (color) (pos.at(2) / 3);
+        //while the cubie is not at its solved state
+        while(faceWhite != WHITE || faceCol != col){
+            pos = c->locateCubie(WHITE, (color) col);
+            faceWhite = (color) (pos.at(0) / 3);
+            faceCol = (color) (pos.at(2) / 3);
+            switch(faceWhite){
+            case WHITE:
+                //if the cubie is on the white face but not at its correct place
+                c->turnFace(faceCol);
+                c->turnFace(faceCol);
+                break;
+            case YELLOW:
+                //if it's on the yellow face
+                if(faceCol == col){
+                    //if it's on the correct face
+                    c->turnFace((color) col);
+                    c->turnFace((color) col);
+                } else {
+                    //if its not, we turn the face until it isc
+                    c->turnFace(YELLOW);
+                }
+                break;
+            default:
+                //if the white sticker is not on the white or yellow face
+                if(col == faceCol){
+                    //if it's just on quarter turn away from being correctly placed
+                    c->turnFace((color) col);
+                } else if (pos.at(1) == 1){
+                    //if it is on any of the 4 edges, we turn the face it's on so the white sticker is on the yellow face
+                    if(pos.at(2) > faceCol * 3 + 1){
+                        c->turnFace((faceCol));
+                        c->turnFace(YELLOW);
+                        c->turnFace(YELLOW);
+                        c->turnFace((faceCol));
+                        c->turnFace((faceCol));
+                        c->turnFace((faceCol));
+                    } else {
+                        c->turnFace((faceCol));
+                        c->turnFace((faceCol));
+                        c->turnFace((faceCol));
+                        c->turnFace(YELLOW);
+                        c->turnFace(YELLOW);
+                        c->turnFace((faceCol));
+                    }
+                } else if (faceCol == WHITE){
+                    //if the col sticker is on white
+                    //we do the algorithm
+                    c->turnFace((color)col);
+                    c->turnFace(WHITE);
+                    c->turnFace(WHITE);
+                    c->turnFace(WHITE);
+                    pos = c->locateCubie(WHITE, (color) col);
+                    faceWhite = (color) (pos.at(0) / 3);
+                    faceCol = (color) (pos.at(2) / 3);
+                    c->turnFace(faceCol);
+                    c->turnFace(WHITE);
+                } else if (faceWhite != col) {
+                    //it's on the yellow face but not where we want it
+                    c->turnFace(YELLOW);
+                } else {
+                    //we do the algorithm to solve the cubie
+                    c->turnYellow(1);
+                    pos = c->locateCubie(WHITE, (color) col);
+                    faceWhite = (color) (pos.at(0) / 3);
+                    faceCol = (color) (pos.at(2) / 3);
+                    c->turnFace(faceWhite);
+                    c->turnFace((color) col);
+                    c->turnFace((color) col);
+                    c->turnFace((color) col);
+                    c->turnFace(faceWhite);
+                    c->turnFace(faceWhite);
+                    c->turnFace(faceWhite);
+                }
+                break;
+            }
+            pos = c->locateCubie(WHITE, (color) col);
+            faceWhite = (color) (pos.at(0) / 3);
+            faceCol = (color) (pos.at(2) / 3);
+        }
+    }
+    return s;
 }
 
 //For now, it's only solving for the white face
--- code/TURBOCUBEsolver/Fridrich.h
+++ code/TURBOCUBEsolver/Fridrich.h
@@ -9,18 +9,18 @@ using namespace CubeColor;
 class Fridrich
 {
 public:
-    QString solve(Cube *c);
+    static QString solve(Cube *c);
 
 private:
     //int face is the face we begin with,
     //usually white so we set it at 0
     //by default
-    QString cross(Cube *c);
-    QString F2L(Cube *c);
+    static QString cross(Cube *c);
+    static QString F2L(Cube *c);
     //as 2 layers are solved at this point,
     //we don't need to set a face to solve
-    QString OLL(Cube *c);
-    QString PLL(Cube *c);
+    static QString OLL(Cube *c);
+    static QString PLL(Cube *c);
 };
 
 #endif // FRIDRICH_H
