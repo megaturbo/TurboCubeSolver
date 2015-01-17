9f3b476838d030fab382231178889a696c376659
Thomas Roulin <thomas.roulin@he-arc.ch>
2015-01-17 17:38:25 +0100

    cleaned main

--- code/TURBOCUBEsolver/main.cpp
+++ code/TURBOCUBEsolver/main.cpp
@@ -16,65 +16,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    /*CubeInput u;
-    u.show();*/
-
-    /*FRIDRICH TESTING*/
-
-//    qDebug() << "Fridrich testing, please do not comment or remove this section";
-//    color solvedMatrix[18][3];
-
-//    for (int x = 0; x < 18; ++x) {
-//        for (int y = 0; y < 3; ++y) {
-//            solvedMatrix[x][y] = (color)(x / 3);
-//        }
-//    }
-//    Cube *testCube = new Cube(solvedMatrix);
-
-
-
-//    testCube->displayCube();
-//    for (int col = 5; col > -1; --col) {
-//        testCube->turnFace((color)col, -1);
-//        testCube->displayCube();
-//    }
-
-//    testCube->displayCube();
-
-//    QString s = Fridrich::solve(testCube);
-
-//    testCube->displayCube();
-
-//    qDebug() << s;
-
-    // DISPLAY ISOMETRIC CUBE
-
-    // get color matrix
-
-    // create the isometric widget
-//    Cube *solvedCube = new Cube(solvedMatrix);
-
-//    QString scrambling = solvedCube->scramble();
-//    QString scrambling = "L F2 B U D R' L' F2 D2 F' L2 U2 B F' L' U' D R U F' ";
-//    solvedCube->moveSequence(scrambling);
-
-//    qDebug() << "SCRAMBLE: " << scrambling;
-//    qDebug() << "UNSCRAMBLE: " << Cube::reverseSequence(scrambling);
-
-//    IsometricCubeWidget w(*solvedCube);
-
-//    QString solv = Fridrich::solve(solvedCube);
-
-//    w.setCube(*solvedCube);
-
-//    qDebug() << solv;
-
-//    qDebug() << "SCRAMBLE: " << scrambling;
-
-//    w.show();
-
-    //testCube->turnFace(WHITE,1);
-    //w.setCube(*testCube);
     MainWidget mainWidget;
     mainWidget.show();
 
