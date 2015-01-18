00bed3aaaf647386bb7b65dc754aadedef24094e
Thomas Roulin <thomas.roulin@he-arc.ch>
2015-01-18 17:49:05 +0100

    User interface, when you solve you can make move by move -> solved cube

--- code/TURBOCUBEsolver/MainWidget.cpp
+++ code/TURBOCUBEsolver/MainWidget.cpp
@@ -10,7 +10,9 @@ MainWidget::MainWidget(QWidget *parent) :
     // Display widget
     isometricCubeWidget = new IsometricCubeWidget(*displayedCube);
 
-    // Menu
+    //=====         BOTTOM MENU         =====//
+
+    // Instanciation
     scramblePB = new QPushButton("Scramble", this);
     cubeInputPB = new QPushButton("Set configuration", this);
     solvePB = new QPushButton("Solve", this);
@@ -19,46 +21,93 @@ MainWidget::MainWidget(QWidget *parent) :
     sequenceLE = new QLineEdit(this);
     reverseSequencePB = new QPushButton("R", this);
 
-    // Menu: orientation
-    movesPB = new QPushButton*[3]();
 
+    //=====     RESOLUTION MENU        =====//
+
+    // Instanciation
+    pastMovesLabel = new QLabel(this);
+    nextMovesLabel = new QLabel(this);
+    actMoveLabel = new QLabel(this);
+    pastMovePB = new QPushButton("<", this);
+    nextMovePB = new QPushButton(">", this);
+
+    // Settings
+    pastMovePB->setMaximumWidth(40);
+    nextMovePB->setMaximumWidth(40);
+    pastMovePB->setDisabled(TRUE);
+    nextMovePB->setDisabled(TRUE);
+    sResolution = new QStringList();
+    actMoveID = 0;
+
+    // Settings: widths
+    nextMovesLabel->setMaximumWidth(350);
+    pastMovesLabel->setMaximumWidth(350);
+    actMoveLabel->setMaximumWidth(80);
+
+    // Settings: Fonts
+    QFont f = actMoveLabel->font();
+    f.setPointSize(36);
+    actMoveLabel->setFont(f);
+    actMoveLabel->setAlignment(Qt::AlignHCenter);
+    nextMovesLabel->setAlignment(Qt::AlignLeft);
+    pastMovesLabel->setAlignment(Qt::AlignRight);
+
+
+    //=====        RIGHT MENU           =====//
+
+    // Instanciation
+    movesPB = new QPushButton*[3]();
     movesPB[0] = new QPushButton("x", this);
     movesPB[1] = new QPushButton("y", this);
     movesPB[2] = new QPushButton("z", this);
 
+    // Settings
     movesPB[0]->setMaximumWidth(40);
     movesPB[1]->setMaximumWidth(40);
     movesPB[2]->setMaximumWidth(40);
 
-    // Layouts
-    QHBoxLayout *menuLayout = new QHBoxLayout();
+
+    //=====          LAYOUTS           =====//
+    // Instanciation
+    QHBoxLayout *cubeMenuLayout = new QHBoxLayout();
     QHBoxLayout *topLayout = new QHBoxLayout();
-    QVBoxLayout *MainLayout = new QVBoxLayout();
     QVBoxLayout *orientationMenuLayout = new QVBoxLayout();
+    QHBoxLayout *resolutionMenuLayout = new QHBoxLayout();
+    QVBoxLayout *MainLayout = new QVBoxLayout();
 
-    // Layout: Menu
-    menuLayout->addWidget(cubeInputPB);
-    menuLayout->addWidget(scramblePB);
-    menuLayout->addWidget(solvePB);
-    menuLayout->addWidget(resetPB);
-    menuLayout->addWidget(sequenceLE);
-    menuLayout->addWidget(sequencePB);
-    menuLayout->addWidget(reverseSequencePB);
+    // Cube tools
+    cubeMenuLayout->addWidget(cubeInputPB);
+    cubeMenuLayout->addWidget(scramblePB);
+    cubeMenuLayout->addWidget(solvePB);
+    cubeMenuLayout->addWidget(resetPB);
+    cubeMenuLayout->addWidget(sequenceLE);
+    cubeMenuLayout->addWidget(sequencePB);
+    cubeMenuLayout->addWidget(reverseSequencePB);
 
-    // Layout: orientation menu
+    // Orientation tools
     orientationMenuLayout->addWidget(movesPB[0]);
     orientationMenuLayout->addWidget(movesPB[1]);
     orientationMenuLayout->addWidget(movesPB[2]);
 
+    // Top: IsometricWidget + Orientation Menu
     isometricCubeWidget->setMinimumHeight(600);
     topLayout->addWidget(isometricCubeWidget);
     topLayout->addLayout(orientationMenuLayout);
 
+    // Resolution menu
+    resolutionMenuLayout->addWidget(pastMovesLabel);
+    resolutionMenuLayout->addWidget(pastMovePB);
+    resolutionMenuLayout->addWidget(actMoveLabel);
+    resolutionMenuLayout->addWidget(nextMovePB);
+    resolutionMenuLayout->addWidget(nextMovesLabel);
+
     // Layout: Total
     MainLayout->addLayout(topLayout);
-    MainLayout->addLayout(menuLayout);
+    MainLayout->addLayout(resolutionMenuLayout);
+    MainLayout->addLayout(cubeMenuLayout);
 
-    // CONNECT
+
+    //=====          CONNECTs         =====//
     connect(scramblePB, SIGNAL(clicked()), this, SLOT(scrambleSlot()));
     connect(solvePB, SIGNAL(clicked()), this, SLOT(solveSlot()));
     connect(sequencePB, SIGNAL(clicked()), this, SLOT(sendSequenceSlot()));
@@ -72,10 +121,69 @@ MainWidget::MainWidget(QWidget *parent) :
     connect(cubeInputPB, SIGNAL(clicked()), this, SLOT(startCubeInput()));
     connect(isometricCubeWidget, SIGNAL(cubieModified(int, int, color)), this, SLOT(cubieModified(int, int, color)));
 
+    connect(pastMovePB, SIGNAL(clicked()), this, SLOT(pastMove()));
+    connect(nextMovePB, SIGNAL(clicked()), this, SLOT(nextMove()));
+
     // Display settings
     this->setLayout(MainLayout);
     this->resize(1000,600);
     this->show();
+}
+
+
+
+/**********************************************************\
+|******************       Slots        ********************|
+\******************  a lot of slots    ********************/
+
+void MainWidget::refreshResolutionState()
+{
+    actMoveLabel->clear();
+    pastMovesLabel->clear();
+    nextMovesLabel->clear();
+    nextMovePB->setDisabled(TRUE);
+    pastMovePB->setDisabled(TRUE);
+
+    if(sResolution->length() > 0){
+        // Enable/Disable past/next buttons
+        if(actMoveID < sResolution->length())
+        {
+            nextMovePB->setEnabled(TRUE);
+        }
+        if(actMoveID>0)
+        {
+            pastMovePB->setEnabled(TRUE);
+        }
+
+        // Refresh the display
+        for(int i = 0; i < sResolution->length(); i++)
+        {
+            if(i < actMoveID)
+            {
+                pastMovesLabel->setText(pastMovesLabel->text() + " " + sResolution->at(i));
+            }else if( i > actMoveID){
+                nextMovesLabel->setText(nextMovesLabel->text() + " " + sResolution->at(i));
+            }else{
+                actMoveLabel->setText(sResolution->at(i));
+            }
+        }
+    }
+}
+
+void MainWidget::pastMove()
+{
+    displayedCube->moveSequence(Cube::reverseSequence(actMoveLabel->text()), RED, YELLOW);
+    isometricCubeWidget->setCube(*displayedCube);
+    actMoveID--;
+    refreshResolutionState();
+}
+
+void MainWidget::nextMove()
+{
+    displayedCube->moveSequence(actMoveLabel->text(), RED, YELLOW);
+    isometricCubeWidget->setCube(*displayedCube);
+    actMoveID++;
+    refreshResolutionState();
 }
 
 void MainWidget::cubieModified(int x, int y, color c)
@@ -126,6 +234,10 @@ void MainWidget::reverseSequenceSlot()
 
 void MainWidget::resetSlot()
 {
+    actMoveID = 0;
+    sResolution->clear();
+    refreshResolutionState();
+
     initSolvedCube();
     isometricCubeWidget->setCube(*displayedCube);
 }
@@ -138,7 +250,12 @@ void MainWidget::scrambleSlot()
 
 void MainWidget::solveSlot()
 {
-    Fridrich::solve(displayedCube);
+    actMoveID = 0;
+    Cube *tmpCube = new Cube(*displayedCube);
+    QString res = Fridrich::solve(tmpCube);
+    sResolution = new QStringList(res.split(' '));
+    refreshResolutionState();
+
     isometricCubeWidget->setCube(*displayedCube);
 }
 
