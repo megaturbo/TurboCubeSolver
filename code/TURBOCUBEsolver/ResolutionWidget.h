d90d34eda070c55cee20c40610badbc71925f7f8
Thomas Roulin <thomas.roulin@he-arc.ch>
2015-01-19 15:24:24 +0100

    Work on resolution widget, conflict mergin, idk....

---
+++ code/TURBOCUBEsolver/ResolutionWidget.h
@@ -0,0 +1,34 @@
+#ifndef RESOLUTIONWIDGET_H
+#define RESOLUTIONWIDGET_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+
+class ResolutionWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ResolutionWidget(QWidget *parent = 0);
+
+public slots:
+    void newSolveSequence(QString);
+
+signals:
+    void sendMove(QString);
+
+private:
+    QPushButton *nextMovePB;
+    QPushButton *pastMovePB;
+    QLabel *actMoveLabel;
+    QLabel *crossLabel;
+    QLabel *f2lLabel;
+    QLabel *ollLabel;
+    QLabel *pllLabel;
+    QStringList *resolutionSequence;
+    int actMoveID;
+
+};
+
+#endif // RESOLUTIONWIDGET_H
